358da81cfce62794282199cc569638f668aae3e8
edzion <alle020916@gmail.com>
2021-02-09 11:08:56 +0100

    feat(login.js): added supersecret

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET Login */
 router.get('/', function(req, res, next) {
-  res.render('login');
+  res.render('login',{title: 'Schoolsoft'});
+});
+
+/*  POST Login */
+router.post('/', function(req, res, next) {
+
+  console.log(req.body);
+
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (password == "abc123"){
+    res.send('Hemligt');
+  } else{
+    res.redirect('/login');
+  }
 });
 
 module.exports = router;
