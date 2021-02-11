ac0133fa0b3f1a2ac045fe3f967c7a14633b3613
edzion <alle020916@gmail.com>
2021-02-11 09:35:59 +0100

    feat: adds express-session
    refactor: login rout to use session
    
    feat: adds topsecret route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.get('/', function(req, res, next){
+  res.render('index',{title: 'Topsecret'})
+});
+
+/* GET Topsecret */
+router.get('/topsecret', function(req, res, next) {
+  if (req.session.loggedin){
+    res.send('Du är inloggad');
+  } else{
+    resizeBy.send('Please login to view this page!');
+  }
 });
 
 module.exports = router;
--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET Login */
 router.get('/', function(req, res, next) {
-  res.render('login',{title: 'Schoolsoft'});
+  res.render('login', {title: 'Schoolsoft'});
 });
 
 /*  POST Login */
@@ -15,8 +15,9 @@ router.post('/', function(req, res, next) {
   const password = req.body.password;
 
   if (password == "abc123"){
-    res.send('Hemligt');
-    
+    req.session.loggedin = true;
+    req.session.username = username;
+    res.redirect('/topsecret');
   } else{
     res.render('login', {title :'Schoolsoft', error :'FEL!'});
   }
