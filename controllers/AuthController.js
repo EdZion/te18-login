3eb94f612f91f91c7bdc3bf33826b758b4dba9bc
edzion <alle020916@gmail.com>
2021-02-19 10:03:46 +0100

    controller

---
+++ controllers/AuthController.js
@@ -0,0 +1,40 @@
+const bcrypt = require('bcrypt');
+const { query } = require('../models/db');
+const { body, validationResult } = require('express-validator');
+
+module.exports.show = async function(req, res, next)  {
+  return res.render('login');
+};
+
+module.exports.store = async function(req, res, next) {
+  // Finds the validation errors in this request and wraps them in an object with handy functions
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.render('login',{ errors: errors.array()});
+    // return res.status(400).json({ errors: errors.array() });
+  }
+  const username = req.body.username;
+  const password = req.body.password;
+
+  try {
+    const sql = 'SELECT password FROM users WHERE name = ?';
+    const result = await query(sql, username);
+
+    if(result.length > 0) {
+      bcrypt.compare(password, result[0].password, function(err, result) {
+        if (result == true) {
+          req.session.loggedin = true;
+          req.session.username = username;
+          res.redirect('/home');
+        } else {
+          res.render('login',{ errors: 'Wrong username or password!'});
+        }
+      });
+    } else {
+      res.render('login',{ errors: 'Wrong username or password!'});
+    }
+  } catch (e) {
+    next(e);
+    console.error(e);
+  }
+};
--- routes/login.js
+++ routes/login.js
@@ -3,12 +3,10 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const { query } = require('../models/db');
 const { body, validationResult } = require('express-validator');
+const AuthController = require('../controllers/AuthController');
 
 /* GET login form */
-router.get('/', function(req, res, next) {
-  // res.send('respond with a resource');
-  res.render('login', {title: 'Schoolsoft'});
-});
+router.get('/', AuthController.show);
 
 /* GET skapa en hash */
 router.get('/kryptan/:pwd', function(req, res, next) {
@@ -27,40 +25,10 @@ router.get('/kryptan/:pwd', function(req, res, next) {
 
 /* POST login */
 router.post('/',
-  body('username').notEmpty().trim(),
-  body('password').notEmpty(),
-  async function(req, res, next) {
-  // Finds the validation errors in this request and wraps them in an object with handy functions
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    res.render('login',{ errors: errors.array()});
-    // return res.status(400).json({ errors: errors.array() });
-  }
-  const username = req.body.username;
-  const password = req.body.password;
-
-  try {
-    const sql = 'SELECT password FROM users WHERE name = ?';
-    const result = await query(sql, username);
-
-    if(result.length > 0) {
-      bcrypt.compare(password, result[0].password, function(err, result) {
-        if (result == true) {
-          req.session.loggedin = true;
-          req.session.username = username;
-          res.redirect('/home');
-        } else {
-          res.render('login',{ errors: 'Wrong username or password!'});
-        }
-      });
-    } else {
-      res.render('login',{ errors: 'Wrong username or password!'});
-    }
-  } catch (e) {
-    next(e);
-    console.error(e);
-  }
-});
+body('username').notEmpty().trim(),
+body('password').notEmpty(),
+AuthController.store
+);
 
 
 module.exports = router;
